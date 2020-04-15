00312ed2a423ddd943f19c24d86af2346336bd5c
unknown <tonymkcv@naver.com>
2020-04-15 18:07:52 +0900

    깃페이지 테스트

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Test from './test';
+
+function App() {
+  return (
+    <div className="App">
+      <Test></Test>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/test.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class test extends Component {
+    render() {
+        return (
+            <div>
+                <h1>깃페이지 테스트</h1>
+            </div>
+        )
+    }
+}
