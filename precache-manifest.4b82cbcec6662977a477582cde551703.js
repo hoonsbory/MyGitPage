230974d218d4751513e51e5b8ebcc290ecdb0e1b
unknown <tonymkcv@naver.com>
2020-04-15 18:09:01 +0900

    Updates

---
+++ precache-manifest.4b82cbcec6662977a477582cde551703.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee6511e65b9ce6eb88871987bf24b62",
+    "url": "/MyGitPage/index.html"
+  },
+  {
+    "revision": "77627a650ce1a40ddffd",
+    "url": "/MyGitPage/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7a2d3380201772b414e7",
+    "url": "/MyGitPage/static/js/2.a5333e8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyGitPage/static/js/2.a5333e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77627a650ce1a40ddffd",
+    "url": "/MyGitPage/static/js/main.79cc5fdd.chunk.js"
+  },
+  {
+    "revision": "cb1cf676857237c5997a",
+    "url": "/MyGitPage/static/js/runtime-main.ea6e46b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyGitPage/static/media/logo.5d5d9eef.svg"
+  }
+]);
