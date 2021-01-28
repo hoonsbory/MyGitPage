936012346ed1a590ac13422ce05f53403d69f555
hoonsbory <tonymkcv@naver.com>
2021-01-28 20:12:27 +0900

    .

--- src/component/projectImg.js
+++ src/component/projectImg.js
@@ -3,12 +3,12 @@ import React, { useEffect } from 'react'
 
 const ProjectImg = ({ project, length }) => {
     // var map = length.map((x,idx)=> <div><img alt="projectImg" id={"projectImg"+idx} className="projectImg" src={`https://hoonsbory.github.io/MyGitPage/images/${project}0${idx+1}.jpg`} /></div>)
-    var map = length.map((x, idx) => <div><img alt="projectImg" id={"projectImg" + idx} className="projectImg" src={`/MygitPage/images/loading.svg`} /></div>)
+    var map = length.map((x, idx) => <div><img alt="projectImg" className={"projectImg" + idx + " projectImg"} src={`/MyGitPage/images/loading.svg`} /></div>)
     const settings = {
 
         customPaging: function (i) {
             return (
-                <img alt="previewImg" id={"projectDockImg" + i} src={`/MygitPage/images/loading.svg`} width="40px" height="40px" />
+                <img alt="previewImg" className={"projectDockImg" + i} src={`/MyGitPage/images/loading.svg`} width="40px" height="40px" />
             );
         },
         dots: true,
@@ -20,16 +20,27 @@ const ProjectImg = ({ project, length }) => {
     };
     useEffect(() => {
         if (!project) return
+        //
         length.forEach((i,idx)=>{
-            document.getElementById("projectImg"+idx).src = `/MyGitPage/images/loading.svg`
-            document.getElementById("projectDockImg"+idx).src = `/MyGitPage/images/loading.svg`
-            var img = new Image();
-            img.onload = function () {
-                document.getElementById("projectImg"+idx).src = img.src
-                document.getElementById("projectDockImg"+idx).src = img.src
-            };
-            img.src = `/MyGitPage/images/${project}0${idx+1}.jpg`
-        })
+            [...document.getElementsByClassName("projectImg"+idx)].forEach(x=>{
+                x.src = `/MyGitPage/images/loading.svg`
+                var img = new Image();
+                img.onload = function () {
+                    x.src = img.src
+                };
+                img.src = `/MyGitPage/images/${project}0${idx+1}.jpg`
+            });
+
+            [...document.getElementsByClassName("projectDockImg"+idx)].forEach(x=>{
+                x.src = `/MyGitPage/images/loading.svg`
+                var img = new Image();
+                img.onload = function () {
+                    x.src = img.src
+                };
+                img.src = `/MyGitPage/images/${project}0${idx+1}.jpg`
+            })
+
+            })
     }, [project])
 
     return (
